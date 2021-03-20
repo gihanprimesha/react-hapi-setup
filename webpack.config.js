094ff6b1d88c7a37af64cd225bb4ced96e58be4d
Gihan Primesha <gihan@ideahub.lk>
2021-03-20 11:59:23 +0530

    Server creations

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 'use strict';
-const webpack = require('webpack'),
-    path = require('path');
+
+const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const BUILD_DIR = path.resolve(__dirname, 'public/dist/');
 const APP_DIR = path.resolve(__dirname, 'public/');
 
@@ -13,7 +15,7 @@ module.exports = {
 
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js',
+        filename: 'bundle1222.js',
         publicPath: '/',
     },
     resolve: {
